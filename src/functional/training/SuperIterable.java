7b12e76da947cc0e55a5a3be42897ad16f1af1d8
rida safwan <ridasaf1@hotmail.com>
2025-10-30 16:11:45 +0200

    adding filter method to SuperIterable<E>. This filter accepts a Predicate<E>

--- src/functional/training/SuperIterable.java
+++ src/functional/training/SuperIterable.java
@@ -1,7 +1,10 @@
 package functional.training;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
 
 /*
  * Here we are wrapping an iterable in another iterable
@@ -20,14 +23,38 @@ public class SuperIterable<E> implements Iterable<E>{
 		return self.iterator();
 	}
 	
+	public SuperIterable<E> filter(Predicate<E> pred) {
+		List<E> results = new ArrayList<>();
+		
+		for(E e: self) {
+			if(pred.test(e)) {
+				results.add(e);
+			}
+		}
+		return new SuperIterable<>(results);
+	}
+	
 	
 	public static void main(String[] args) {
 		//build SuperIterable<E>:
 		SuperIterable<String> strings = new SuperIterable<String>(
-				Arrays.asList("LightCoral", "pink", "Orange", "Gold", "plum", "Blue", "LimeGreen"));
+				Arrays.asList("LightCoral", "pink", "Orange", "Gold", "plum", "Blue", "limeGreen"));
 		
 		//as strings is iterable => we are able to iterate on it:
 		for(String s: strings) {
+			System.out.println("> " + s);
+		}
+		Predicate<String> upperCasePredicate = (s) -> {
+			if(Character.isUpperCase(s.charAt(0))) {
+				return true;
+			}
+			return false;
+		};
+		
+		System.out.println("-------------------------------------------------");
+		SuperIterable<String> upperCase = strings.filter(upperCasePredicate);
+		
+		for(String s: upperCase) {
 			System.out.println("> " + s);
 		}
 	}
